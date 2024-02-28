6591fc121bc1e6dd02cda8755ede3ece33c3f69d
Giang Minh <giangndm@ows.vn>
2024-02-28 15:25:50 +0700

    WIP: test bus/leg

--- src/bus/leg.rs
+++ src/bus/leg.rs
@@ -4,7 +4,7 @@ use std::sync::Arc;
 use crate::collections::DynamicDeque;
 
 /// Represents the identifier of a channel.
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Clone)]
 pub enum BusEventSource<ChannelId> {
     Channel(usize, ChannelId),
     Broadcast(usize),
@@ -95,4 +95,59 @@ pub fn create_bus_leg<ChannelId, MSG, const STATIC_SIZE: usize>() -> (
     };
     let receiver = BusLegReceiver { queue };
     (sender, receiver)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_send_and_recv() {
+        // Create a bus leg
+        let (sender, receiver) = create_bus_leg::<u32, String, 10>();
+
+        // Send a message
+        let source = BusEventSource::Channel(0, 42);
+        let msg = "Hello, world!".to_string();
+        let result = sender.send(source.clone(), true, msg.clone());
+        assert_eq!(result, Ok(1));
+
+        // Receive the message
+        let received = receiver.recv();
+        assert_eq!(received, Some((source, msg)));
+    }
+
+    #[test]
+    fn test_send_channel_full() {
+        // Create a bus leg with a static size of 1
+        let (sender, receiver) = create_bus_leg::<u32, &'static str, 1>();
+        let source = BusEventSource::Channel(0, 42);
+
+        // Send the first message
+
+        let result = sender.send(source.clone(), false, "Message 1");
+        assert_eq!(result, Ok(1));
+
+        // Send the second message (should fail)
+        let result = sender.send(source.clone(), false, "Message 2");
+        assert_eq!(result, Err(BusLegSenderErr::ChannelFull));
+
+        // Send the second message as safe (should not fail)
+        let result = sender.send(source.clone(), true, "Message 3");
+        assert_eq!(result, Ok(2));
+
+        assert_eq!(receiver.recv(), Some((source.clone(), "Message 1")));
+        assert_eq!(receiver.recv(), Some((source, "Message 3")));
+        assert_eq!(receiver.recv(), None);
+    }
+
+    #[test]
+    fn test_recv_empty_queue() {
+        // Create a bus leg
+        let (_, receiver) = create_bus_leg::<u32, String, 10>();
+
+        // Receive a message from an empty queue
+        let received = receiver.recv();
+        assert_eq!(received, None);
+    }
 }
